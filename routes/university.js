97fb461c24a8ab4ee40ed8c13f2b8ead20db9cc0
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2021-04-14 22:49:33 -0500

    improvised using css and added code to display no instance when null

--- routes/university.js
+++ routes/university.js
@@ -15,6 +15,8 @@ router.get('/create', university_controllers.university_create_Page);
 /* GET create update page */
 router.get('/update', university_controllers.university_update_Page);
 
+/* GET create costume page */
+router.get('/delete', university_controllers.university_delete_Page);
 
 
 module.exports = router;
